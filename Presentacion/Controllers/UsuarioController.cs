7078b8327452f1b4b3c0b170ceb560d72f0520ee
alvarogarciagrana <alvarogarciagrana@gmail.com>
2020-04-14 17:24:07 +0200

    Cambios alvaro

--- Presentacion/Controllers/UsuarioController.cs
+++ Presentacion/Controllers/UsuarioController.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using Presentacion.Extensions.Controllers;
 
 namespace Presentacion.Controllers
 {
-    public class UsuarioController : Controller
+
+    [RoutePrefix("api/usuario")]
+    public class UsuarioController : ApiBaseController
     {
         public ActionResult Index()
         {
@@ -25,6 +29,14 @@ namespace Presentacion.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost, Route("registro")]
+        public ActionResult RegistrarUsuario(string nombre, string pass)
+        {
+            //User usuario = usuarioServicio.Identificacion(nombre, pass);
+            DataTable result = new DataTable();
+            return result != null ? JsonSuccess(result) : JsonError("Error al cargar usuario");
         }
     }
 }
