e9567425e3399826ce1e87881b0576d4a944af50
ManuelAntonioRegueiraSomoza <62422574+ManuelAntonioRegueiraSomoza@users.noreply.github.com>
2020-04-14 16:17:51 +0200

    DAOTransaccionInicio

---
+++ Datos/Conexion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Conexion
+    {
+        private String NombreConexion;
+
+        public Conexion()
+        {
+            NombreConexion = "Data Source=PLX300000000420\\SQLEXPRESS;Initial Catalog=PartidosDB;Integrated Security=True";
+        }
+
+        public string GetNombreConexion()
+        {
+            return this.NombreConexion;
+        }
+    }
+}
---
+++ Datos/DatosTransaccion/DTOTransaccion.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.DatosTransaccion
+{
+   public class DTOTransaccion
+    {
+        private int IdTransaccion;
+        private float Importe;
+        private int IdMonedero;
+        private DateTime FechaCreacion;
+        private String TipoTransaccion;
+
+        public DTOTransaccion()
+        {
+
+        }
+        public DTOTransaccion(int idTransaccion, int idMonedero, float importe, DateTime fechaCreacion, string tipoTransaccion)
+        {
+            IdTransaccion = idTransaccion;
+            IdMonedero = idMonedero;
+            Importe = importe;
+            FechaCreacion = fechaCreacion;
+            TipoTransaccion = tipoTransaccion;
+        }
+
+        public int GetIdTransaccionDTO()
+        {
+            return this.IdTransaccion;
+        }
+        public void SetIdTransaccionDTO(int id)
+        {
+            this.IdTransaccion = id;
+        }
+        public int GetIdMonederoDTO()
+        {
+            return this.IdMonedero;
+        }
+        public void SetIdMonederoDTO(int id)
+        {
+            this.IdMonedero = id;
+        }
+        public float GetImporteDTO()
+        {
+            return this.Importe;
+        }
+        public void SetImporteDTO(float imp)
+        {
+            this.Importe = imp;
+        }
+        public DateTime GetFechaCreacionDTO()
+        {
+            return this.FechaCreacion;
+        }
+        public void SetFechaCreacionDTO(DateTime fecha)
+        {
+            this.FechaCreacion = fecha;
+        }
+
+        public string GetTipoTransaccionDTO()
+        {
+            return this.TipoTransaccion;
+        }
+        public void SetTipoTransaccionDTO(string tipo)
+        {
+            this.TipoTransaccion = tipo;
+        }
+    }
+}
