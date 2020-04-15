8cc3e8027876dbec1c3063f3503d5711849f0c42
alvarogarciagrana <alvarogarciagrana@gmail.com>
2020-04-15 19:30:08 +0200

    recoger y subir cambios alvaro

---
+++ CelulaRiego/App/js/webAPI.js
@@ -0,0 +1,73 @@
+﻿//DIRECCIÓN LOCAL
+var raizUrl = '';
+
+/*USUARIOS*/
+
+function indentificar(vnombre, vpass) {
+
+    $.ajax({
+        url: raizUrl + '/api/usuarios/indentificacion',
+        type: 'POST',
+        dataType: 'json',
+        cache: false,
+        data: { nombre: vnombre, pass: vpass },
+        success: function (objeto) {
+            localStorage.setItem("idUsuario", objeto.data[0].id);
+            localStorage.setItem("nombreUsuario", objeto.data[0].nombre);
+        }
+    });
+}
+
+function enviarRegistro(usuario, password) {
+    $.ajax({
+        url: raizUrl + '/api/usuarios/registro',
+        type: 'POST',
+        dataType: 'json',
+        cache: false,
+        data: { nombre: usuario, pass: password },
+        success: function (objeto) {
+            //var usuario = objeto.data;
+            if (objeto.data.length > 0) {
+                if (parseInt(objeto.data[0].id) > 0) {
+                    //localStorage.setItem("idUsuario", objeto.data[0].id);
+                    //localStorage.setItem("nombreUsuario", objeto.data[0].nombre);
+                    //window.location.href = "partidos.html";
+                } else {
+                    alert('Error de conexión a BBDD.');
+                }
+            } else {
+                alert('Usuario Incorrecto.');
+            }
+        }
+    });
+}
+
+function enviarInicioSesion(usuario, password) {
+    /*$.ajax({
+        url: raizUrl + '/api/usuario/inicioSesion',
+        type: 'POST',
+        dataType: 'json',
+        cache: false,
+        data: { nombre: usuario, pass: password },
+        success: function (objeto) {
+            //var usuario = objeto.data;
+            if (objeto.data.length > 0) {
+                if (parseInt(objeto.data[0].id) > 0) {
+                    //localStorage.setItem("idUsuario", objeto.data[0].id);
+                    //localStorage.setItem("nombreUsuario", objeto.data[0].nombre);
+                    //window.location.href = "partidos.html";
+                } else {
+                    alert('Error de conexión a BBDD.');
+                }
+            } else {
+                alert('Usuario Incorrecto.');
+            }
+        }
+    });*/
+    console.log(usuario, password);
+    location.href = "../Transaccion/registroTransaccion.html"
+}
+
+function generarSesion() {
+    location.href = "../App/Usuario/registroUsuario.html"
+}
---
+++ CelulaRiego/App_Start/IdentityConfig.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+
+namespace CelulaRiego
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Conecte su servicio de correo electrónico aquí para enviar correo electrónico.
+            return Task.FromResult(0);
+        }
+    }
+
+    public class SmsService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Conecte el servicio SMS aquí para enviar un mensaje de texto.
+            return Task.FromResult(0);
+        }
+    }
+
+}
