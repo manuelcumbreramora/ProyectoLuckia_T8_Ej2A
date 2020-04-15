4ebfcb0fe40ac9fc4d8c22bca6a1e8f23836b4ae
Rubenncio <49519667+Rubenncio@users.noreply.github.com>
2020-04-15 13:50:10 +0200

    Cambios;

---
+++ Datos/DatosMonedero/DAOIMPLMonedero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class DAOIMPLMonedero : IDAOMonedero
+{
+    float IDAOMonedero.ComprobarSaldo(DTOMonedero monedero)
+    {
+        throw new NotImplementedException();
+    }
+
+    void IDAOMonedero.CrearMonedero(DTOMonedero monedero)
+    {
+        throw new NotImplementedException();
+    }
+
+    float IDAOMonedero.restarSaldo(DTOMonedero monedero)
+    {
+        throw new NotImplementedException();
+    }
+
+    float IDAOMonedero.sumarSaldo(DTOMonedero monedero)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Negocio/Monedero.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+ public class Monedero
+    {
+        public float saldo { get; set; }
+        public int IdMonedero { get; set; }
+        public string tipo { get; set; }
+
+        public Monedero(float saldo, int idMonedero, string tipo)
+        {
+            this.saldo = saldo;
+            IdMonedero = idMonedero;
+            this.tipo = tipo;
+        }
+
+        public Monedero()
+        {
+
+        }
+
+        public void CrearMonedero(float saldo, int IdMonedero)
+        {
+            
+
+        }
+        public float ComprobarSaldo()
+        {
+            return saldo;
+        }
+
+        public float restarSaldo()
+        {
+
+            return saldo;
+        }
+
+        public float sumarSaldo()
+        {
+            
+            return saldo;
+        }
+    }
